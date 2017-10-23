daed56b99952ce0f8fd967495c2e40c1b8f18523
Bashar Natour <bashar.nat1997@gmail.com>
2017-10-24 01:14:07 +0200

    update

--- public/index.js
+++ public/index.js
@@ -5,38 +5,56 @@ const length = images.length;
 const h1     = $("h1");
 
 let i = 3;
-setInterval (function() {
-	let next = i === length ? 1 : i + 1;
-	images.each(function () {
-		$(this).css("z-index", 0);
-	});
-	// images.eq(i - 1)
-	// 	.css({
-	// 		zIndex: 1
-	// 	}).animate({
-	// 		left: "-100%"
-	// 	});
-	// images.eq(next - 1)
-	// 	.css({
-	// 		left: "100%",
-	// 		zIndex: 1
-	// 	}).animate({
-	// 		left: 0
-	// 	});
+let interval = 0;
+let h1Interval = 0;
 
-	images.eq(i - 1)
-		.css("z-index", 1)
-		.fadeOut(600);
-	images.eq(next - 1)
-		.css("z-index", 2)
-		.css("display", "none")
-		.fadeIn(600);
+function start() {
+
+	interval = setInterval (function() {
+		let next = i === length ? 1 : i + 1;
+		images.each(function () {
+			$(this).css("z-index", 0);
+		});
+		// images.eq(i - 1)
+		// 	.css({
+		// 		zIndex: 1
+		// 	}).animate({
+		// 		left: "-100%"
+		// 	});
+		// images.eq(next - 1)
+		// 	.css({
+		// 		left: "100%",
+		// 		zIndex: 1
+		// 	}).animate({
+		// 		left: 0
+		// 	});
+
+		images.eq(i - 1)
+			.css("z-index", 1)
+			.fadeOut(600);
+		images.eq(next - 1)
+			.css("z-index", 2)
+			.css("display", "none")
+			.fadeIn(600);
 
 
-	i = i === length ? 1 : i + 1;
-}, 5000);
+		i = i === length ? 1 : i + 1;
+	}, 5000);
 
-setInterval(function(){
-	h1.fadeToggle(600)
+	h1Interval = setInterval(function(){
+		h1.fadeToggle(600)
+	}, 600);
 }
-, 600);
+
+
+start();
+
+$(window).on("blur", function(){
+  console.log("stopped");
+  clearInterval(interval);
+  clearInterval(h1Interval);
+});
+$(window).on("focus", function(){
+  console.log("started");
+  start();
+});
